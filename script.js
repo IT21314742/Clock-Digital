9243277b4f0cfe654fca0f71397bb609e576b9a6
Vihan Jayasinghe <it21314742@my.sliit.lk>
2024-02-07 10:24:53 +0530

    Update jS
    Update jS

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ setInterval(() => {
     let currentTime = new Date();   
 
     hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-    min.innerHTML = (currentTime.getMinutes()<10?"0":"") +
+    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + 
     
 }, 1000);
