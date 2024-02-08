9ff00d3dcc3160162119766148e147bf0c3573b8
Vihan Jayasinghe <it21314742@my.sliit.lk>
2024-02-08 22:28:34 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
 setInterval(() => {
-    let currentTime = new Date();   
+    let currentTime = new Date();
 
-    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
-    sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
-    
+    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+
 }, 1000);
